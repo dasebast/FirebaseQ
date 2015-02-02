15fc9da008103069ec882b9a68ec8e55f1af9673
jacob-israel-turner <poppa.bear.joe@gmail.com>
2015-02-01 22:30:40 -0700

    Completes Firebase.  Cleans up/comments out code.

--- js/app.js
+++ js/app.js
@@ -1,9 +1,11 @@
-var app = angular.module('parseQ', []);
+var app = angular.module('parseQ', ['firebase']);
 
 app
 	.constant('constants', {
 		"firebaseUrl": "https://brilliant-inferno-4121.firebaseio.com/"
 	})
-	.config(function($httpProvider){
-	  $httpProvider.interceptors.push('httpRequestInterceptor');
-	});
+
+	
+	// .config(function($httpProvider){
+	//   $httpProvider.interceptors.push('httpRequestInterceptor');
+	// });
--- js/mainController.js
+++ js/mainController.js
@@ -2,43 +2,75 @@ var app = angular.module('parseQ');
 
 app.controller('mainCtrl', function($scope, firebaseService){
 	$scope.getData = function() {
-		firebaseService.getQuestions()
-			.then(function(data){
-				$scope.questions = data.data.results;
-			})
+		$scope.questions = firebaseService.getQuestions().$asArray();
+		console.log($scope.questions);
 	}
 	$scope.postData = function(){
 		var obj = {}
 		obj.question = $scope.question;
 		obj.color = 'red';
-		firebaseService.postQuestion(obj)
-			.success(function(){
-				$scope.getData();
-				$scope.question = '';
-			})
-			.error(function(){
-				alert('There was an error.')
-				$scope.getData();
-			})
+		$scope.questions.$add(obj);
+		$scope.question = '';
 	}
-	$scope.updateData = function(data) {
-		data.color = 'yellow'
-		firebaseService.updateQuestion(data)
-			.success(function(){
-				$scope.getData();
-			})
-			.error(function(){
-				alert('There was an error')
-			})
+	$scope.updateData = function(obj) {
+		for(var i = 0; i < $scope.questions.length; i++){
+			if($scope.questions[i].$id === obj.$id){
+				$scope.questions[i].color = 'yellow';
+				$scope.questions.$save(obj);
+				return;				
+			}
+		}
 	}
-	$scope.deleteData = function(data) {
-		firebaseService.deleteQuestion(data)
-			.success(function(){
-				$scope.getData();
-			})
-			.error(function(){
-				alert('There was an error')
-			})
+	$scope.deleteData = function(obj) {
+		for(var i = 0; i < $scope.questions.length; i++){
+			if($scope.questions[i].$id === obj.$id){
+				$scope.questions.$remove(i);
+				return;				
+			}
+		}
 	}
+
 	$scope.getData();
+
+
+	// $scope.getData = function() {
+	// 	firebaseService.getQuestions()
+	// 		.then(function(data){
+	// 			$scope.questions = data.data.results;
+	// 		})
+	// }
+	// $scope.postData = function(){
+	// 	var obj = {}
+	// 	obj.question = $scope.question;
+	// 	obj.color = 'red';
+	// 	firebaseService.postQuestion(obj)
+	// 		.success(function(){
+	// 			$scope.getData();
+	// 			$scope.question = '';
+	// 		})
+	// 		.error(function(){
+	// 			alert('There was an error.')
+	// 			$scope.getData();
+	// 		})
+	// }
+	// $scope.updateData = function(data) {
+	// 	data.color = 'yellow'
+	// 	firebaseService.updateQuestion(data)
+	// 		.success(function(){
+	// 			$scope.getData();
+	// 		})
+	// 		.error(function(){
+	// 			alert('There was an error')
+	// 		})
+	// }
+	// $scope.deleteData = function(data) {
+	// 	firebaseService.deleteQuestion(data)
+	// 		.success(function(){
+	// 			$scope.getData();
+	// 		})
+	// 		.error(function(){
+	// 			alert('There was an error')
+	// 		})
+	// }
+	// $scope.getData();
 })
